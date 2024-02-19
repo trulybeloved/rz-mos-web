03fbb21139611c51da4de0bf8842c314aacad710
beloved <toshiroakari@gmail.com>
2024-02-19 21:56:03 +0530

    Title change.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheHeader from './components/TheHeader.vue';
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/rz_mos_web_logo.svg" width="125" height="125" />
+    <img alt="logo" class="logo" src="./assets/rz_mos_web_logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <TheHeader />
