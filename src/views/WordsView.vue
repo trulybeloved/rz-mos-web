0a72798b1774d5c0b6f3a8397dab6104033cbe14
beloved <toshiroakari@gmail.com>
2024-02-28 15:22:01 +0530

    Added loading spinner

--- src/views/WordsView.vue
+++ src/views/WordsView.vue
@@ -1,11 +1,15 @@
 <script setup>
 import WordsDeck from '@/components/WordsDeck.vue'
+import LoadingStuff from '@/components/LoadingStuff.vue'
 </script>
 
 <template>
   <main>
     <Suspense>
       <WordsDeck />
+      <template #fallback>
+        <LoadingStuff />
+      </template>
     </Suspense>
   </main>
 </template>
